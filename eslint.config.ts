6573c99292d45a15af6b50f8dc2e3eff83045959
Yuki <admin@yuki.sh>
2026-04-24 14:07:37 +0800

    chore: add ESLint configuration

---
+++ eslint.config.ts
@@ -0,0 +1,14 @@
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    ignores: ['build/**', 'node_modules/**'],
+  },
+  {
+    rules: {},
+  },
+);
